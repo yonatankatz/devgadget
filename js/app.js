609d151a1a24b468ee74ae588c925c930588e51d
Yonatan <katzyonatan@gmail.com>
2017-05-08 23:24:57 +0300

    add default-action directive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 //Module
+window.getParameterByName = function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
 var app = angular.module('app', []);
 
 app.directive("menu", function() {
@@ -20,23 +30,37 @@ app.directive("analytics", function() {
           restrict : "A",
           link: function(scope, element, attrs) {
           // see http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-            function getParameterByName(name, url) {
-                if (!url) url = window.location.href;
-                name = name.replace(/[\[\]]/g, "\\$&");
-                var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-                    results = regex.exec(url);
-                if (!results) return null;
-                if (!results[2]) return '';
-                return decodeURIComponent(results[2].replace(/\+/g, " "));
-            }
 
-            value = getParameterByName('q');
+
+            value = window.getParameterByName('q');
             $timeout(function() {
                     if (value) {
                         element[0].value = value;
                     }
                     element[0].focus();
                 }, 1);
+          }
+       };
+   });
+
+app.directive("defaultAction", function($interval) {
+       return {
+          restrict : "A",
+          link: function(scope, element, attrs) {
+            value = window.getParameterByName('q');
+            if (!value) return;
+            var counter = 0;
+            promise = $interval( function(){
+                counter++;
+                if (counter === 20) {
+                    $interval.cancel(promise);
+                }
+                var elements = document.querySelectorAll("[default-component]");
+                if (elements && elements[0].value == value) {
+                    $interval.cancel(promise);
+                    element[0].click();
+                }
+            }, 200);
           }
        };
    });
