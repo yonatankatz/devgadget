5f24d30fc1dea2a51afbb3ee7df5f415e664f890
Yonatan <katzyonatan@gmail.com>
2017-05-11 07:37:46 +0300

    copy to clipboard

--- js/app.js
+++ js/app.js
@@ -1,4 +1,61 @@
 //Module
+
+// see http://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+window.copyTextToClipboard = function(text) {
+  var textArea = document.createElement("textarea");
+
+  //
+  // *** This styling is an extra step which is likely not required. ***
+  //
+  // Why is it here? To ensure:
+  // 1. the element is able to have focus and selection.
+  // 2. if element was to flash render it has minimal visual impact.
+  // 3. less flakyness with selection and copying which **might** occur if
+  //    the textarea element is not visible.
+  //
+  // The likelihood is the element won't even render, not even a flash,
+  // so some of these are just precautions. However in IE the element
+  // is visible whilst the popup box asking the user for permission for
+  // the web page to copy to the clipboard.
+  //
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = 'transparent';
+  textArea.value = text;
+  document.body.appendChild(textArea);
+
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+
+  document.body.removeChild(textArea);
+};
+
+
 window.getParameterByName = function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -64,3 +121,34 @@ app.directive("defaultAction", function($interval) {
           }
        };
    });
+
+app.directive("selectOnFocus", function($timeout) {
+      return {
+         restrict : "A",
+         link: function(scope, element, attrs) {
+            // see http://stackoverflow.com/questions/480735/select-all-contents-of-textbox-when-it-receives-focus-javascript-or-jquery
+            $timeout(function() {
+            $(element[0]).focus(function(){
+                console.log('ere');
+                $(this).one('mouseup', function(event){
+                    event.preventDefault();
+                }).select();
+            });
+            }, 1);
+        }
+      }
+});
+
+app.directive("copyToClipboard", function($timeout) {
+      return {
+         restrict : "A",
+         link: function(scope, element, attrs) {
+            $(element[0]).click(function() {
+                if (!document.getElementById('output')) return;
+                var val = document.getElementById('output').value
+                if (!val) return;
+                window.copyTextToClipboard(val);
+            });
+        }
+      }
+});
