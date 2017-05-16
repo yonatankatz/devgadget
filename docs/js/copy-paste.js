9f1c12c5c8ac5d5ec56dbdbf814127ab05c9ec4d
Yonatan <katzyonatan@gmail.com>
2017-05-16 23:46:50 +0300

    add copy paste

--- docs/js/copy-paste.js
+++ docs/js/copy-paste.js
@@ -2,7 +2,39 @@ var copyText = document.getElementById('copyText');
 var outputLink = document.getElementById('outputLink');
 var output = document.getElementById('output');
 var input = document.getElementById('input');
-var outputLinkDiv = document.getElementById('outputLinkDiv');
+var error = document.getElementById('errorMsg');
+
+var initErrorMsg = function () {
+   if (outputLink.value) {
+        error.innerHTML = '';
+   }
+};
+
+window.onload = function () {
+
+	if (outputLink.addEventListener) {
+		// event handling code for sane browsers
+		outputLink.addEventListener('input', initErrorMsg, false);
+	}
+	else if (input.attachEvent) {
+		// IE-specific event handling code
+		input.attachEvent('onpropertychange', initErrorMsg);
+	}
+};
+
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+function formatToken(token) {
+    return token.replaceAll('.', '-')
+                .replaceAll('#', '-')
+                .replaceAll('[', '-')
+                .replaceAll(']', '-')
+                .replaceAll('%', '-');
+}
 
 function makeToken()
 {
@@ -15,11 +47,17 @@ function makeToken()
     return text;
 }
 
-var currentToken = makeToken();
+var currentToken = localStorage.getItem("copyPasteToken");
+if (!currentToken) {
+    currentToken = makeToken();
+}
+
+document.getElementById('baseLink').innerHTML = location.protocol + '//' + location.host + location.pathname + '?' ;
+outputLink.value = currentToken;
 
 var config = {
     apiKey: "AIzaSyA_KOm82nLzVd2S-8sXsj76APHvrBNevYk",
-    authDomain: " devgadget-36dfb.firebaseapp.com",
+    authDomain: "devgadget-36dfb.firebaseapp.com",
     databaseURL: "https://devgadget-36dfb.firebaseio.com/",
     storageBucket: "copyPaste.appspot.com"
 };
@@ -29,7 +67,7 @@ var database = firebase.database();
 
 var token = window.location.search.substr(1);
 if (token) {
-    firebase.database().ref(token).once('value').then(function(storedValue) {
+    firebase.database().ref(formatToken(token)).once('value').then(function(storedValue) {
         if (!storedValue.val() || !storedValue.val().value) {
             console.log('no value found for token ' + token);
             return;
@@ -42,18 +80,23 @@ else {
     $('#copyMode').removeClass('hide');
 }
 
-function writeUserData(value) {
-    firebase.database().ref(currentToken).set({
-      token: currentToken,
+function writeUserData(value, token) {
+    firebase.database().ref(formatToken(token)).set({
+      token: token,
       value: value,
+      time: new Date().toString()
     });
-    return currentToken;
 }
 
 copyText.onclick = function() {
         if (!input.value) return;
+        if (!outputLink.value) {
+            error.innerHTML = 'Link token cannot be empty';
+            return;
+        }
 		var str = input.value;
-		var token = writeUserData(str);
-		$('#outputLinkDiv').removeClass('hide');
-		outputLink.value = location.protocol + '//' + location.host + location.pathname + '?' + token;
+		var token = outputLink.value;
+		localStorage.setItem("copyPasteToken" , token);
+		writeUserData(str, token);
+		$('#textCopiedMsg').removeClass('hide');
 };
